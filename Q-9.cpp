4004673790ec2f0a874448183f43a4892ed1ae95
Anas-Altaf <anashafiz926@gmail.com>
2023-04-02 22:49:09 -0700

    Update Q-9.cpp

--- Q-9.cpp
+++ Q-9.cpp
@@ -1,1 +1,50 @@
 
+#include<iostream>
+#include<Windows.h>
+using namespace std;
+//=> PERSON
+class Person {
+
+};
+//--> Student Work
+class Student :public Person {
+
+};
+//---->Math Student
+class MathStudent :public Student {
+
+};
+//----> CS Student
+class CSStudent :public Student {
+
+};
+//--> Employee Work
+class Employee:public Person {
+
+};
+//----> Professor
+class Professor :public Employee {
+
+};
+//------> Department Chairman
+class DepartmentChair :public Professor {
+
+};
+//----> Staff
+class Staff :public Employee {
+
+};
+//------> Manager
+class Manager :public Staff {
+
+};
+//------> IT Worker
+class ITWorker :public Staff {
+
+};
+int main()
+{
+
+	system("pause");
+	return 0;
+}
