14d40d33833ec2040fce8d23794a33f8fb582d54
Anas-Altaf <anashafiz926@gmail.com>
2023-04-13 00:17:13 -0700

    Update Q-9.cpp

--- Q-9.cpp
+++ Q-9.cpp
@@ -1,18 +1,53 @@
-
 #include<iostream>
 #include<Windows.h>
+#include<string>
 using namespace std;
+
 //=> PERSON
 class Person {
-
+private:
+	string name;
+	string age;
+	string addr;
+protected:
+	void input1() {
+		cout << "Enter name = "; cin >> name;
+		cout << "enter age = "; cin >> age;
+		cout << "Enter Address  ="; cin >> addr;
+	}
+	void output1() {
+		cout << "Name is = " << name << endl;
+		cout << "Age is = " << age << endl;
+		cout << "Address is = " << addr << endl;
+	}
 };
 //--> Student Work
 class Student :public Person {
-
+private :
+	string rollno;
+protected:
+	void input2() {
+		input1();
+		cout << "Enter Roll No = "; cin >> rollno;
+	}
+	void output2() {
+		output1();
+		cout << "Roll No is = " << endl;
+	}
 };
 //---->Math Student
 class MathStudent :public Student {
-
+private:
+	string coursename;
+public:
+	void input3() {
+		input2();
+		cout << "Enter Math Course Name = "; cin >> coursename;
+	}
+	void output3() {
+		output2();
+		cout << "Course Name is = " << coursename << endl;
+	}
 };
 //----> CS Student
 class CSStudent :public Student {
@@ -44,6 +79,9 @@ class ITWorker :public Staff {
 };
 int main()
 {
+	MathStudent obj1;
+	obj1.input3();
+	obj1.output3();
 
 	system("pause");
 	return 0;
